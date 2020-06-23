1c91e0ffbdd4bcddb48507eda8589e4d11346476
Gaurav Chalana <gauravchalana.ece@gmail.com>
2020-06-23 16:48:10 -0500

    Structure and Static data

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
+    new Recipe('A test Recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
